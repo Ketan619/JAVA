dfd33f35187bda7e53fdf809d9da1c7049351b02
Ketan619 <ketanvishwakarma619@gmail.com>
2025-04-17 14:50:33 +0530

    all programs

---
+++ carstatic.java
@@ -0,0 +1,16 @@
+
+public class carstatic {
+
+    String name , colour, model;
+
+
+    public static void main(String[] args) {
+
+        carstatic obj = new carstatic();
+        obj.colour= "Red ";
+        obj.name= " Audi ";
+        obj.model= " XR1024PC23 ";
+        System.out.println(obj.colour+ obj.model+ obj.name);
+
+    }
+}
