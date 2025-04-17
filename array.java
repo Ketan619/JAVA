5e0c3a59e2d2b5f19c74c67faed35472970ecfe4
Ketan619 <ketanvishwakarma619@gmail.com>
2025-04-17 15:53:38 +0530

    all programs

--- array.java
+++ array.java
@@ -2,23 +2,30 @@ import java.util.Scanner;
 
 public class array {
     public static void main(String[] args) {
-        Scanner sc=new Scanner(System.in);
-        int sum=0;
-System.out.println("Enter the size of array");
-int n=sc.nextInt();
-System.out.println("enter ar valuess");
-int[]arr=new int[n];
-for (int i=0;i<n;i++){
-    arr[i]=sc.nextInt();
+        Scanner sc = new Scanner(System.in);
+        int sum = 0;
 
-}
-for (int j=0;j<arr.length;j++){
-    System.out.println(arr[j]);
-    sum+=arr[j];
-    max=Math.mac(max.arr[j];)
-}
+        System.out.println("Enter the size of array");
+        int n = sc.nextInt();
 
-System.out.println(sum);
-System.out.println(max);
+        System.out.println("Enter array values");
+        int[] arr = new int[n];
+
+        for (int i = 0; i < n; i++) {
+            arr[i] = sc.nextInt();
+        }
+
+        int max = arr[0]; // Pehle element ko max maan ke chalu karte hain
+
+        for (int j = 0; j < arr.length; j++) {
+            System.out.println(arr[j]);
+            sum += arr[j];
+            if (arr[j] > max) {
+                max = arr[j]; // Update max if current element is greater
+            }
+        }
+
+        System.out.println("Sum: " + sum);
+        System.out.println("Maximum: " + max);
     }
 }
