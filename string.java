f9194a1cacb00c0318ae854906c2b406297692ba
Ketan619 <ketanvishwakarma619@gmail.com>
2025-04-28 13:33:40 +0530

    all programs

---
+++ string.java
@@ -0,0 +1,40 @@
+// public class string {
+//    public static void main(String[] args) {
+//     String str="hello";
+//     String newStr="";
+//     for (int i = 0; i < str.length(); i++) {
+//         char c=str.charAt(i);
+//         if (newStr.indexOf(c)==-1) {
+//             newStr+=c;
+//         }
+//     }
+    
+//    } 
+// }
+public class string{
+    public static void main(String[] args) {
+        // String str="EE SALA ";
+        // String str2="CUP NAMDE";
+        // System.out.println(str.concat(str2));
+        // System.out.println(str.length());
+        // System.out.println(str.toUpperCase());
+        // System.out.println(str.replace("h","k"));
+        // StringBuilder sb = new StringBuilder(3);
+        // sb.append("RCB18");
+        // StringBuilder sb = new StringBuilder("Royal");
+        // sb.append("Challanger");
+        // sb.append("will ");
+        // sb.append("win ");
+        // sb.append("the trophy!");
+        
+        // System.out.println(sb.toString());
+        // System.out.println(sb.capacity());
+int arr[]={1,3,4,6};
+System.out.println("YOUR ARRAY IS  :");
+for (int i = 0; i < arr.length; i++) {
+    System.out.println(arr[i]);
+}
+
+    }
+
+}
