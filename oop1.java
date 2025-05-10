91199701f38878834c9063ec8202260b9141fc60
Ketan619 <ketanvishwakarma619@gmail.com>
2025-05-10 21:07:57 +0530

    all programs

---
+++ oop1.java
@@ -0,0 +1,27 @@
+
+class Student{
+    String name="EE SALA CUP LOLLYPOPU.....!";
+    void add (int a,int b){
+System.out.println(a+b);
+    }
+}
+class fullName{
+    String fristname="Virat";
+    String lastName="Kholi";
+    void full() {
+    System.out.print(fristname+lastName);
+    }
+}
+public class oop1 {
+    public static void main(String[] args) {
+        Student obj =new Student ();
+        Student obj1 =new Student ();
+        System.out.println(obj1.name);
+        obj.add(4, 5);
+        fullName name =new fullName();
+        name.full();
+
+
+    }
+}
+
