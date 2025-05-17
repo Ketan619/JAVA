8e64092d25250903441822858cb8193f1922c5d9
Ketan619 <ketanvishwakarma619@gmail.com>
2025-05-17 13:26:12 +0530

    all programs

--- 05_Arrays_And_Searching/Add_2array.java
+++ 05_Arrays_And_Searching/Add_2array.java
@@ -1,4 +1,3 @@
-
 import java.util.*;
 public class Add_2array {
     public static void main(String[] args) {
@@ -16,3 +15,21 @@ public class Add_2array {
     }
     
 }
+// package Array;
+// import java.util.*;
+// public class Add_2array {
+//     public static void main(String[] args) {
+//         Scanner sc = new Scanner(System.in);
+//         int[] arr1={1,3,5,7,9};
+//         int[] arr2={2,4,6,8,10};
+//         int[] sum=new int [arr1.length];
+//         for(int i=0;i<arr1.length;i++){
+//             sum[i]=arr1[i]+arr2[i];
+//         }
+//         for(int i=0;i<arr1.length;i++){
+//             System.out.println("Sum is : "+ sum[i]);
+//         }
+//         sc.close();
+//     }
+    
+// }
