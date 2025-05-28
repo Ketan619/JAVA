a07ee224fbd7191a6bfe3a85a19b325796807994
Ketan619 <ketanvishwakarma619@gmail.com>
2025-05-28 13:15:44 +0530

    all programs

--- 12_Java_DSA_Concepts/Hash_Map.java
+++ 12_Java_DSA_Concepts/Hash_Map.java
@@ -4,9 +4,14 @@ public class Hash_Map {
  public static void main(String[] args) {
     HashMap<String,Integer> map=new HashMap<>();
     map.put("id1",1);
-    map.put("id2",1);
-    map.put("id3",1);
-    map.put("id4",1);
-    System.out.println("id0");
+    map.put("id2",2);
+    map.put("id3",3);
+    map.put("id4",4);
+    System.out.println(map);
+    // System.out.println(map.get("id2"));
+    System.out.println(map.getOrDefault("id5", 5));
+    // System.out.println("show values "+map.values());
+    // System.out.println("show keys "+map.keySet());
+
  }   
 }
