e587e69d07cf0ea178e50433990cbd89576d778b
Ketan619 <ketanvishwakarma619@gmail.com>
2025-05-28 13:41:05 +0530

    all progrmas

--- 12_Java_DSA_Concepts/Hash_Map.java
+++ 12_Java_DSA_Concepts/Hash_Map.java
@@ -7,11 +7,18 @@ public class Hash_Map {
     map.put("id2",2);
     map.put("id3",3);
     map.put("id4",4);
-    System.out.println(map);
-    // System.out.println(map.get("id2"));
-    System.out.println(map.getOrDefault("id5", 5));
-    // System.out.println("show values "+map.values());
-    // System.out.println("show keys "+map.keySet());
+     System.out.println(map);
+    System.out.println("specfic"+ map.get("id4"));//specfic
+    System.out.println(map.getOrDefault("id5", 5));//not in the list defalut value
+     System.out.println(map.getOrDefault("id1", null));//if in the list print the value
+    System.out.println("show all values "+map.values());
+     System.out.println("show keys "+map.keySet());
+HashMap<Integer, String> map1 = new HashMap<>();
+map1.put(101, "Virat");
+map1.put(102, "ABD");
 
+System.out.println(map1.get(101)); // ➤ Output: Virat
+System.out.println(map1.values()); // ➤ Output: [Virat, ABD]
  }   
 }
+
---
+++ 12_Java_DSA_Concepts/Hash_problem.java
@@ -0,0 +1,13 @@
+import java.util.HashMap;
+
+public class Hash_problem {
+    public static void main(String[] args) {
+        String str="PROGRAMMING";
+     HashMap<Character,Integer>map=new HashMap<>();
+     for (char ch:str.toCharArray()){
+        map.put(ch,map.getOrDefault(ch, 0)+1);
+
+     }
+     System.out.println(map);
+    }
+}
